8a23a544824a136602cfdaf8fd510789674774fa
syehan269 <syehan269@gmail.com>
2023-06-06 17:20:04 +0700

    update logout handler

--- handlers/Auth/logout.js
+++ handlers/Auth/logout.js
@@ -1,11 +1,13 @@
-import supabaseClient from "../../utils/supabaseClient";
+import supabaseClient from '../../utils/supabaseClient.js';
 
 const logout = async (request, h) => {
-    const {error} = await supabaseClient.auth.signOut();
+  const { error } = await supabaseClient.auth.signOut();
 
-    if (error) {
-        return h.response(error).code(500);
-    }
+  if (error) {
+    return h.response(error).code(500);
+  }
 
-    return h.response({message: 'logout success'}).code(201);
-}
+  return h.response({ message: 'logout success' }).code(201);
+};
+
+export default logout;
