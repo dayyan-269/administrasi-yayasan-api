8a07014d36ce3dd7d1931c894c7de5400446df38
Dayyan Syehan <101689434+dayyan-269@users.noreply.github.com>
2023-06-14 11:22:41 +0700

    minor fix

--- Schemas/JenisKetunaanSchema.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-const JenisKetunaanSchema = Joi.object({
-    nama: Joi.string().required()
-});
-
-export default JenisKetunaanSchema;
--- Schemas/KebutuhanMedisSchema.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-const KebutuhanMedisSchema = Joi.object({
-    nama: Joi.string().required(),
-});
-
-export default KebutuhanMedisSchema;
