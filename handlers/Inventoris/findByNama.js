b80b0a9a225428b6de6f712ddeb294d6ea9b888d
Yoris22 <yulianusbetan@gmail.com>
2023-06-20 19:57:08 +0700

    remove inventori modul

---
+++ handlers/Inventoris/findByNama.js
@@ -0,0 +1,24 @@
+import * as Boom from "@hapi/boom";
+import supabaseClient from "../../utils/supabaseClient.js";
+
+const findInventorisByNama = async (Request, h) => {
+  const { nama } = Request.query;
+
+  const findInventoris = await supabaseClient
+    .form("inventoris")
+    .select()
+    .ilike("nama", `%${nama}`);
+
+  if (findInventoris.error) {
+    throw new Boom.internal(findInventoris.error);
+  }
+
+  return h
+    .response({
+      message: "find success",
+      data: findInventoris.nama,
+    })
+    .code(200);
+};
+
+export default findInventorisByNama;
---
+++ handlers/Inventoris/list.js
@@ -0,0 +1,19 @@
+import * as Boom from "@hapi/boom";
+import supabaseClient from "../../utils/supabaseClient.js";
+
+const listInventoris = async (Request, h) => {
+  const result = await supabaseClient.from("inventoris").select();
+
+  if (result.error) {
+    throw Boom.internal(result.error);
+  }
+
+  return h
+    .response({
+      message: "fetch success",
+      data: result.data,
+    })
+    .code(200);
+};
+
+export default listInventoris;
