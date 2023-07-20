ec3571e8fa408f797c6c4268bbeee5887417c311
dayyan-269 <syehan.al.dayyan@student.unmer.ac.id>
2023-07-20 12:26:46 +0700

    minor fix

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const init = async () => {
     port: process.env.PORT || 10000,
     host: process.env.HOST || 'localhost',
     routes: {
-      cors: false,
+      cors: {
+        origin: ['http://localhost:3000'],
+      },
     },
   });
 
