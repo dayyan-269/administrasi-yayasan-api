060d847d269224d62d3bd3341547fae91bace35f
dayyan-269 <syehan.al.dayyan@student.unmer.ac.id>
2023-07-20 13:12:10 +0700

    minor

--- schemas/AnakAsuhanSchema.js
+++ schemas/AnakAsuhanSchema.js
@@ -3,12 +3,12 @@ import Joi from 'joi';
 const AnakAsuhanSchema = Joi.object({
   nama: Joi.string().required(),
   tempat_lahir: Joi.string().optional(),
-  tanggal_lahir: Joi.date().optional(),
+  tanggal_lahir: Joi.optional(),
   asal: Joi.string().required(),
-  tanggal_masuk: Joi.date().required(),
-  tanggal_keluar: Joi.date().optional(),
+  tanggal_masuk: Joi.required(),
+  tanggal_keluar: Joi.optional(),
   tipe_pembayaran_id: Joi.number().required(),
-  jenis_ketunaan: Joi.number().required(),
+  jenis_ketunaan_id: Joi.number().required(),
   tinggi_badan: Joi.number().required(),
   berat_badan: Joi.number().required(),
 });
